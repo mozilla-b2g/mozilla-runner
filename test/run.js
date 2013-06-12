6a88435f020fcba5adb05053993cd206d2275862
James Lal <james@lightsofapollo.com>
2013-06-11 17:45:17 -0700

    add url option

--- test/run.js
+++ test/run.js
@@ -1,19 +1,83 @@
 suite('run', function() {
+  this.timeout('5s');
+
   var run = require('../lib/run').run;
   var runtime = __dirname + '/fixtures/firefox';
-  var profileDir = __dirname + '/fixtures/profile';
   var assert = require('assert');
+  var profile = require('mozilla-profile-builder').firefox.profile;
+  var static = require('node-static');
 
   test('launch firefox', function(done) {
+    var profileDir = __dirname + '/fixtures/profile';
     var options = { profile: profileDir };
     this.timeout(5000);
-    run('firefox', runtime, options, function(err, child) {
+    run('firefox', runtime, options, function(err, child, bin, argv) {
       assert.ok(!err, err && err.message);
+
+      // verify the binary
+      assert.ok(bin.indexOf('firefox-bin') !== 0, 'has bin');
+
+      // verify arguments
+      assert.deepEqual(
+        argv,
+        ['-profile', profileDir, '-no-remote']
+      );
+
+      // verify we have a child
       assert.ok(child.kill, 'is process');
-      setTimeout(function() {
-        child.kill();
+      child.kill();
+      done();
+    });
+  });
+
+  suite('launch firefox read dump', function() {
+    var server;
+    var port = 60033;
+    var config = {
+      // turn on dump
+      'browser.dom.window.dump.enabled': true
+    };
+
+    // create server
+    setup(function() {
+      var file = new (static.Server)(__dirname + '/html');
+      server = require('http').createServer(function(req, res) {
+        // stolen from node-static
+        req.on('end', function() {
+          file.serve(req, res);
+        }).resume();
+      });
+      server.listen(port);
+    });
+
+    // ensure server gets closed
+    teardown(function() {
+      server.close();
+    });
+
+    // create tmp profile
+    var profileDir;
+    setup(function(done) {
+      profile({ userPrefs: config }, function(err, _profileDir) {
+        if (err) return done(err);
+        profileDir = _profileDir;
         done();
-      }, 2000);
+      });
+    });
+
+    var url = 'http://localhost:' + port + '/dump.html';
+    var expected = 'WOOT DUMP';
+    test('go to url', function(done) {
+      var options = { profile: profileDir, url: url };
+      run('firefox', runtime, options, function(err, child) {
+        if (err) return done(err);
+        child.stdout.on('data', function(content) {
+          content = content.toString();
+          // verify we can go to a given url
+          if (content.indexOf(expected) !== -1)
+            return done();
+        });
+      });
     });
   });
 
